00dd0b3eb7285a9fc275e2b7640234ae8f631a27
tiaz0128 <tiaz0128.dev@gmail.com>
2021-08-06 08:41:33 +0900

    Add Test case  When a keydown occurs print console.log keycode

--- cypress/integration/JS30-test/1.spec.js
+++ cypress/integration/JS30-test/1.spec.js
@@ -1,20 +1,19 @@
 
 describe('01 - JavaScript Drum Kit App', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:5500/01%20-%20JavaScript%20Drum%20Kit/index-START.html')
+      cy.visit('http://localhost:5500/01%20-%20JavaScript%20Drum%20Kit/index-START.html',  {
+        onBeforeLoad(win) {
+          cy.stub(win.console, 'log').as('consoleLog')
+        },
+      })
     })
   
     it('9개 key 가 존재', () => {
       cy.get('.key').should('have.length', 9)
     })
 
-    it('9개의 키보드 입력가 발생시 콘솔에 keycode 값 출력', () => {
-      cy.get('body').type('a')//.tigger('keydown', {})
-      // cy.get('.key').then((ele) => {
-      //   const key = ele[random(0, 9)]
-      //   const keyCode = key.dataset.key
-        
-      //   cy.get('body').type(String.fromCharCode(keyCode))
-      // })
+    it('키보드 입력 a가 발생시 keycode 값 65 콘솔출력', () => {
+      cy.get('body').type('a')
+      cy.get('@consoleLog').should('be.calledWith', 65)
     })
 })
