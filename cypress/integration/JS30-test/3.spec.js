0e5d8fc2d212e7e8c968a655f8244a1905955462
tiaz0128 <tiaz0128.dev@gmail.com>
2021-09-08 10:20:08 +0900

    init test

--- cypress/integration/JS30-test/3.spec.js
+++ cypress/integration/JS30-test/3.spec.js
@@ -1,6 +1,12 @@
 
 describe('01 - JavaScript Drum Kit App', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/03%20-%20CSS%20Variables/index-START.html')
+    cy.visit('http://localhost:5500/03%20-%20CSS%20Variables/index-START.html')
   })
+  
+  it('input 3개(Spacing, Blur, Base Color) 존재', () => {
+    cy.get('input').should('have.length', 3)
+  })
+
+
 })
