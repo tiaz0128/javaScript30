f5e39634b7c3044fe17978ef59e32f703607af01
tiaz0128 <tiaz0128.dev@gmail.com>
2021-09-12 00:11:33 +0900

    Base Color 와 같은 background-color 값을 가진다.

--- cypress/integration/JS30-test/3.spec.js
+++ cypress/integration/JS30-test/3.spec.js
@@ -17,9 +17,28 @@ describe('01 - JavaScript Drum Kit App', () => {
   })
 
   it('Blur 값만큼 이미지의 filter 값을 가진다..', () => {
-    cy.get('#spacing').should('have.attr', 'value').then((spaceValue) => {
+    cy.get('#blur').should('have.attr', 'value').then((blurValue) => {
       cy.get('img').invoke('css', 'filter').then((filter) => {
-        expect(filter).to.equal(`blur(${spaceValue}px)`)
+        expect(filter).to.equal(`blur(${blurValue}px)`)
+      })
+    })
+  })
+
+  it('Base Color 와 같은 background-color 값을 가진다.', () => {
+    cy.get('#base').should('have.attr', 'value').then((baseValue) => {
+      cy.get('img').invoke('css', 'background-color').then((backgroundColor) => {
+
+        function covertRGB(baseValue){
+          const RGB = []
+          const [sharp, ...colorValues] = [...baseValue]
+          colorValues.forEach((v, i) => {
+            RGB[Math.floor(i/2)] = parseInt(v, 16) * Math.pow(16, (i + 1) % 2) + (RGB[Math.floor(i/2)] || 0 * 1)
+          })
+
+          return RGB
+        }
+        const [R,G,B] = covertRGB(baseValue)
+        expect(backgroundColor).to.equal(`rgb(${R}, ${G}, ${B})`)
       })
     })
   })
