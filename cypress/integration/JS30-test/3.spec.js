34484327665172294cb3a7b6d9d1d1a4efa20c3d
tiaz0128 <tiaz0128.dev@gmail.com>
2021-09-12 11:02:12 +0900

    Add Test case - input 값 변경에 따른 검증

--- cypress/integration/JS30-test/3.spec.js
+++ cypress/integration/JS30-test/3.spec.js
@@ -1,5 +1,12 @@
+const SPACE_VALUE = 150
+const BLUR_VALUE  = 20
+const BASE_VALUE  = '#FFFFFF'
 
-describe('01 - JavaScript Drum Kit App', () => {
+function covertRGB(baseValue){
+  return baseValue.match(/[^#]{2}/g).map(hex => parseInt(hex, 16))
+}
+
+describe('03 - CSS Variables', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500/03%20-%20CSS%20Variables/index-START.html')
   })
@@ -27,19 +34,25 @@ describe('01 - JavaScript Drum Kit App', () => {
   it('Base Color 와 같은 background-color 값을 가진다.', () => {
     cy.get('#base').should('have.attr', 'value').then((baseValue) => {
       cy.get('img').invoke('css', 'background-color').then((backgroundColor) => {
-
-        function covertRGB(baseValue){
-          const RGB = []
-          const [sharp, ...colorValues] = [...baseValue]
-          colorValues.forEach((v, i) => {
-            RGB[Math.floor(i/2)] = parseInt(v, 16) * Math.pow(16, (i + 1) % 2) + (RGB[Math.floor(i/2)] || 0 * 1)
-          })
-
-          return RGB
-        }
         const [R,G,B] = covertRGB(baseValue)
         expect(backgroundColor).to.equal(`rgb(${R}, ${G}, ${B})`)
       })
     })
+  })
+
+  it('Spacing-input 에 입력된 값만큼 이미지의 padding 값을 가진다.', () => {
+    cy.get('#spacing').invoke('val', SPACE_VALUE).trigger('change')
+    cy.get('img').invoke('css', 'padding').should('eq', `${SPACE_VALUE}px`)  
+  })
+
+  it('Blur-input 에 입력된 값만큼 이미지의 filter 값을 가진다.', () => {
+    cy.get('#blur').invoke('val', BLUR_VALUE).trigger('change')
+    cy.get('img').invoke('css', 'filter').should('eq', `blur(${BLUR_VALUE}px)`)  
+  })
+
+  it('Base-input 에 입력된 RGB 색상값으로 이미지의 background-color 값을 가진다.', () => {
+    cy.get('#base').invoke('val', BASE_VALUE).trigger('change')
+    const [R,G,B] = covertRGB(BASE_VALUE)
+    cy.get('img').invoke('css', 'background-color').should('eq', `rgb(${R}, ${G}, ${B})`)  
   })
 })
